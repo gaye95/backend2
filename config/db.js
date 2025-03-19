ace5d018990ff055da6005a6cba0f6d929980d49
Ibra02025 <balise963@gmail.com>
2025-03-19 11:26:53 +0000

    ajou

--- config/db.js
+++ config/db.js
@@ -2,7 +2,9 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://localhost:27017/etudiants"); // ✅ Suppression des options obsolètes
+    await mongoose.connect(
+      "mongodb+srv://ooggaye:passer123@inserer.4vcsc.mongodb.net/?retryWrites=true&w=majority&appName=inserer"
+    ); // ✅ Suppression des options obsolètes
     console.log("✅ MongoDB connecté avec succès !");
   } catch (error) {
     console.error("❌ Erreur de connexion à MongoDB :", error);
